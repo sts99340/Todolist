a089fdea8997c97246726a6313f99193d16b1dd3
sts99340 <123031585+sts99340@users.noreply.github.com>
2025-02-03 10:53:00 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-alert('Hello, World!');
+alert("Hello, World!");
 const task = document.getElementById('task'),
     add = document.getElementById('add'),
     list = document.getElementById('list');
 
     task.addEventListener('keypress', function(e) {
-        if(e.key === 'Enter') {
+        if(e.key === "Enter") {
             addUpdateClick.click();
         }
     });
@@ -16,9 +16,24 @@ const task = document.getElementById('task'),
         }
 
         let li = document.createElement('li');
-        const taskitem = <div>$(task.value)</div>;
+        const taskitem = document.createElement('div');
+        taskitem.className = 'taskitem';
+        const taskText = document.createElement('div');
+        taskText.textContent = task.value;
+        const taskActions = document.createElement('div');
+        taskActions.className = 'taskActions';
+        const editImg = document.createElement('img');
+        editImg.className = 'change';
+        editImg.src = 'images/edit.png';
+        const deleteImg = document.createElement('img');
+        deleteImg.className = 'change';
+        deleteImg.src = 'images/delete.png';
+        taskActions.appendChild(editImg);
+        taskActions.appendChild(deleteImg);
+        taskitem.appendChild(taskText);
+        taskitem.appendChild(taskActions);
 
-        li.innerHTML = taskitem;
+        li.appendChild(taskitem);
         list.appendChild(li);
         task.value = '';
     }
