f83529e9ccfd95604d3efba1141a449f8b555894
sts99340 <123031585+sts99340@users.noreply.github.com>
2025-02-04 10:01:02 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-alert("Hello, World!");
+
 const task = document.getElementById('task'),
     add = document.getElementById('add'),
     list = document.getElementById('list');
 
     task.addEventListener('keypress', function(e) {
         if(e.key === "Enter") {
-            addUpdateClick.click();
+            CreateTask().click();
         }
     });
 
@@ -13,27 +13,89 @@ const task = document.getElementById('task'),
         if (task.value ==="") {
             alert("Please enter a task");
             task.focus();
+        } else{
+            let li = document.createElement('li');
+            const taskitem = document.createElement('div');
+            taskitem.className = 'taskitem';
+            const taskText = document.createElement('div');
+            taskText.textContent = task.value;
+            const taskActions = document.createElement('div');
+            taskActions.className = 'taskActions';
+            const editImg = document.createElement('img');
+            editImg.className = 'change';
+            editImg.src = 'images/edit.png';
+            editImg.addEventListener('click', function() {
+                UpdateTask(li);
+            });
+            const deleteImg = document.createElement('img');
+            deleteImg.className = 'change';
+            deleteImg.src = 'images/delete.png';
+            deleteImg.addEventListener('click', function() {
+                list.removeChild(li);
+            });
+            taskActions.appendChild(editImg);
+            taskActions.appendChild(deleteImg);
+            taskitem.appendChild(taskText);
+            taskitem.appendChild(taskActions);
+
+            li.appendChild(taskitem);
+            list.appendChild(li);
+            task.value = '';
         }
-
-        let li = document.createElement('li');
-        const taskitem = document.createElement('div');
-        taskitem.className = 'taskitem';
-        const taskText = document.createElement('div');
-        taskText.textContent = task.value;
-        const taskActions = document.createElement('div');
-        taskActions.className = 'taskActions';
-        const editImg = document.createElement('img');
-        editImg.className = 'change';
-        editImg.src = 'images/edit.png';
-        const deleteImg = document.createElement('img');
-        deleteImg.className = 'change';
-        deleteImg.src = 'images/delete.png';
-        taskActions.appendChild(editImg);
-        taskActions.appendChild(deleteImg);
-        taskitem.appendChild(taskText);
-        taskitem.appendChild(taskActions);
-
-        li.appendChild(taskitem);
-        list.appendChild(li);
-        task.value = '';
     }
+
+    function UpdateTask(li) {
+        const taskitem = li.querySelector('.taskitem');
+        li.removeChild(taskitem);
+        const editItem = document.createElement('div');
+        editItem.className = 'editItem';
+        const editInput = document.createElement('input');
+        editInput.type = 'text';
+        editInput.className = 'editInput';
+        editInput.value = taskitem.querySelector('div').textContent;
+        const save = document.createElement('img');
+        save.src = 'images/save.png';
+        save.className = 'savebutton';
+        save.addEventListener('click', function() {
+            UpdatedTask(li);
+        });
+        editItem.appendChild(editInput);
+        editItem.appendChild(save);
+
+        li.appendChild(editItem);
+
+    }
+
+    function UpdatedTask(li) {
+        const editInput = li.querySelector('.editItem input');
+        if (editInput.value === "") {
+            list.removeChild(li);
+        } else {
+            const taskitem = document.createElement('div');
+            taskitem.className = 'taskitem';
+            const taskText = document.createElement('div');
+            taskText.textContent = editInput.value;
+            const taskActions = document.createElement('div');
+            taskActions.className = 'taskActions';
+            const editImg = document.createElement('img');
+            editImg.className = 'change';
+            editImg.src = 'images/edit.png';
+            editImg.addEventListener('click', function() {
+                UpdateTask(li);
+            });
+            const deleteImg = document.createElement('img');
+            deleteImg.className = 'change';
+            deleteImg.src = 'images/delete.png';
+            deleteImg.addEventListener('click', function() {
+                list.removeChild(li);
+            });
+            taskActions.appendChild(editImg);
+            taskActions.appendChild(deleteImg);
+            taskitem.appendChild(taskText);
+            taskitem.appendChild(taskActions);
+
+            li.innerHTML = '';
+            li.appendChild(taskitem);
+        }
+    }
+    
